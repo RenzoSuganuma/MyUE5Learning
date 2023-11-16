a66154f8684486cd025fe11a1c7f6f4d7781775c
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2023-11-16 21:58:48 +0900

    [++] SMコンポーネントの追加

---
+++ Source/CPlusPlus_Tutorial/CPPComponent.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPPComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+// Sets default values
+ACPPComponent::ACPPComponent()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	DefaultSceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
+
+	RootComponent = DefaultSceneRoot;
+
+	sMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
+
+	UStaticMesh* mesh = LoadObject<UStaticMesh>(NULL, TEXT("/Game/LevelPrototyping/Meshes/SM_Cube"), NULL, LOAD_None, NULL);
+	sMesh->SetStaticMesh(mesh);
+
+	sMesh->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ACPPComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FString sMessage = "C++ HELLO F***ING WORLD";
+
+	UKismetSystemLibrary::PrintString(
+		this,
+		sMessage,
+		1,
+		1,
+		FColor::Cyan,
+		5.f,
+		TEXT("None"));
+}
+
+// Called every frame
+void ACPPComponent::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/CPlusPlus_Tutorial/CPPComponent.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CPPComponent.generated.h"
+
+UCLASS()
+class CPLUSPLUS_TUTORIAL_API ACPPComponent : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACPPComponent();
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USceneComponent> DefaultSceneRoot;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UStaticMeshComponent> sMesh;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
