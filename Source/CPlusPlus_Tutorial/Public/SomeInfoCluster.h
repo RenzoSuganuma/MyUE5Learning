6a306b88f13b23ed6b6ecfcebc4b5dd6cb355298
MSI_LapTop_RenzoSuganuma <103351677+RenzoSuganuma@users.noreply.github.com>
2023-11-18 19:15:39 +0900

    [**] 構造体草

---
+++ Source/CPlusPlus_Tutorial/Public/SomeInfoCluster.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SomeInfoCluster.generated.h"
+
+USTRUCT(BlueprintType)
+struct FSomeInfoCluster
+{
+	GENERATED_BODY()
+    
+	int32 NumA = 7;
+	int32 NumB = 3;
+};
