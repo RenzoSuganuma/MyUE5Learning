6102e352ec1af50fe5b70e412d09938d78feabfc
MSI_LapTop_RenzoSuganuma <103351677+RenzoSuganuma@users.noreply.github.com>
2023-12-25 17:52:34 +0900

    [update] 継承回りまた学習する

---
+++ Source/CPlusPlus_Tutorial/Public/CPPClassChild.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CPPClassBase.h"
+#include "CPPClassChild.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPLUSPLUS_TUTORIAL_API ACPPClassChild : public ACPPClassBase
+{
+	GENERATED_BODY()
+	
+public:
+	
+};
